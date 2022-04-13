875e80057e3f4a62aa377214af6aa394c81f0df8
Abideen <Abideenali926@gmail.com>
2022-04-13 03:40:20 -0700

    drage & drop programm

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dragdrop from './components/Dragdrop'
+
+function App() {
+  return (
+    <>
+      <Dragdrop />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Dragdrop.jsx
@@ -0,0 +1,61 @@
+import React ,{useState, useEffect} from 'react'
+import './drage.css'
+function Dragdrop() {
+  useEffect(()=>{
+    const fill = document.querySelector('.fill');
+    const empties = document.querySelectorAll('.empty');
+   
+    
+    function dragStart() {
+      fill.className += " hold"
+      setTimeout(()=>fill.className = "invisible",0)
+
+    }
+  
+    function dragEnd() {
+  fill.className = 'fill'
+    }
+  
+    function dragOver(e) {
+      e.preventDefault()
+  
+    }
+    function dragEnter(e) {
+      e.preventDefault()
+      this.className += " hovered"
+    }
+    function dragLeave() {
+      this.className = "empty"
+    }
+    function dragDrop() {
+      empties.className = 'empty';
+     this.append(fill)
+    }
+  
+    fill.addEventListener('dragstart',dragStart)
+    fill.addEventListener("dragend",dragEnd)
+    for(const empty of empties) {
+      empty.addEventListener("dragover",dragOver)
+      empty.addEventListener("dragenter",dragEnter)
+      empty.addEventListener("dragleave",dragLeave)
+      empty.addEventListener("drop",dragDrop)
+    }
+
+  },[])
+  
+  
+  return (
+    <div className="main">
+    <div className="empty">
+        <div className="fill" draggable="true">
+        </div>
+    </div>
+  <div className="empty" ></div>
+  <div className="empty" ></div>
+  <div className="empty" ></div>
+  <div className="empty" ></div>
+</div>
+  );
+}
+
+export default Dragdrop
